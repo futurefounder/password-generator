e745fe50ce38827c94236394802c437e9a27ff5a
futurefounder <jsk@posteo.de>
2022-10-27 11:50:55 +0200

    working

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,27 @@ import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const notify = () => toast("Copied to clipboard");
-  const password = passfather();
-
+  const [password, setPassword] = useState();
+  const [passwordCreated, setPasswordCreated] = useState(true);
   const [passwordConfig, setPasswordConfig] = useState({
-    numbers: false,
-    uppercase: false,
+    numbers: true,
+    uppercase: true,
     lowercase: true,
-    symbols: false, // Disable symbols
+    symbols: true, // Disable symbols
     length: 16,
   });
 
   function makePassword() {
-    setPasswordConfig(password);
+    setPasswordCreated(false);
+    setPassword(passfather(passwordConfig));
   }
   console.log("passwordConfig.numbers: " + passwordConfig.numbers);
-  console.log("password: " + password);
-  console.log("passwordConfig: " + passwordConfig);
+  console.log("passwordConfig.uppercase: " + passwordConfig.uppercase);
+  console.log("passwordConfig.lowercase: " + passwordConfig.lowercase);
+  console.log("passwordConfig.symbols: " + passwordConfig.symbols);
+
   // useMemo
-  const { copied, copy, reset } = useCopy(passwordConfig);
+  const { copied, copy, reset } = useCopy(password);
 
   function handleChange(event: {
     target: { name: any; value: any; type: any; checked: any };
@@ -37,7 +40,6 @@ function App() {
       };
     });
   }
-  // makePassword();
   return (
     <div className="App">
       <div>
@@ -47,14 +49,11 @@ function App() {
             Password Generator{" "}
           </Text>
           <Text align="center" as="p">
-            {() => {
-              setPasswordConfig(password);
-            }}
-            {password}
+            {passwordCreated ? "Press generate " : password}{" "}
             <a
               onClick={() => {
                 copy();
-                // notify();
+                notify();
               }}
             >
               {copied === false ? (
@@ -90,6 +89,7 @@ function App() {
                       d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75"
                     />
                   </svg>
+
                   <ToastContainer
                     position="top-left"
                     autoClose={500}
@@ -136,9 +136,11 @@ function App() {
               </Box>
               <Box mb="xs">
                 <Checkbox
-                  id="cyan"
+                  id="uppercase"
                   color="cyan"
-                  name="cyan"
+                  name="uppercase"
+                  onChange={handleChange}
+                  checked={passwordConfig.uppercase}
                   defaultChecked={true}
                 />
                 <label htmlFor="cyan" className="drac-text drac-text-white">
@@ -147,9 +149,11 @@ function App() {
               </Box>
               <Box mb="xs">
                 <Checkbox
-                  id="green"
+                  id="lowercase"
                   color="green"
-                  name="green"
+                  name="lowercase"
+                  onChange={handleChange}
+                  checked={passwordConfig.lowercase}
                   defaultChecked={true}
                 />
                 <label htmlFor="green" className="drac-text drac-text-white">
@@ -158,9 +162,11 @@ function App() {
               </Box>
               <Box mb="xs">
                 <Checkbox
-                  id="orange"
+                  id="symbols"
                   color="orange"
-                  name="orange"
+                  name="symbols"
+                  onChange={handleChange}
+                  checked={passwordConfig.symbols}
                   defaultChecked={true}
                 />
                 <label htmlFor="orange" className="drac-text drac-text-white">
